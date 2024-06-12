754dd00c254d3924f988fd08eafbcb49f648ff42
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2024-06-13 01:22:21 +0300

    experimental: Add chunk_by, zip, zip_transform, ranges::to, join strings, and sum of squares functions with tests

--- ranges/ranges.cpp
+++ ranges/ranges.cpp
@@ -21,15 +21,21 @@
 #include <algorithm>
 #include <catch2/catch.hpp>
 #include <ranges>
+#include <vector>
 
-constexpr auto is_even = [](int i)
+constexpr auto is_even = [](const auto &i)
 { return i % 2 == 0; };
 
-constexpr auto square = [](int i)
+constexpr auto square = [](const auto &i)
 { return i * i; };
 
 constexpr auto is_equal = [](const auto &a, const auto &b)
 { return a.first == b.first && a.second == b.second; };
+
+constexpr auto are_both_devidede_by_2 = [](const auto &a, const auto &b)
+{
+    return is_even(a) && is_even(b);
+};
 
 // 1. filter
 TEST_CASE("filter")
@@ -86,4 +92,84 @@ TEST_CASE("cartesian product")
 
     REQUIRE(product.size() == expected.size());
     REQUIRE(std::equal(product.begin(), product.end(), expected.begin(), expected.end(), is_equal));
+}
+
+TEST_CASE("chunk by")
+{
+    std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto chunks = arr | std::views::chunk_by(are_both_devidede_by_2);
+
+    REQUIRE(!chunks.empty());
+    auto got_no_chunks = std::ranges::all_of(chunks, [](const auto &chunk)
+                                             { return chunk.size() == 1; });
+    REQUIRE(got_no_chunks);
+}
+
+TEST_CASE("zip")
+{
+    std::array<int, 3> arr1 = {1, 2, 3};
+    std::array<int, 3> arr2 = {4, 5, 6};
+    auto zipped = std::views::zip(arr1, arr2);
+
+    // Convert initializer list to a range
+    std::array<std::pair<int, int>, 3> expected = {
+        std::pair{1, 4}, std::pair{2, 5}, std::pair{3, 6}};
+
+    REQUIRE(zipped.size() == expected.size());
+    REQUIRE(std::equal(zipped.begin(), zipped.end(), expected.begin(), expected.end(), is_equal));
+}
+
+TEST_CASE("zip_transform")
+{
+    std::array<int, 3> arr1 = {1, 2, 3};
+    std::array<int, 3> arr2 = {4, 5, 6};
+    auto zipped = std::views::zip(arr1, arr2) | std::views::transform([](const auto &a)
+                                                                      { return a.first + a.second; });
+
+    // Convert initializer list to a range
+    std::array<int, 3> expected = {5, 7, 9};
+
+    REQUIRE(zipped.size() == expected.size());
+    REQUIRE(std::equal(zipped.begin(), zipped.end(), expected.begin(), expected.end()));
+}
+
+TEST_CASE("ranges::to")
+{
+    //Works in compiler explorer
+    // std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // auto evens = arr | std::views::filter(is_even) | std::ranges::to<std::vector>();
+
+    // // Convert initializer list to a range
+    // std::vector<int> expected = {2, 4, 6, 8, 10};
+
+    // // Compare the filtered view with the range
+    // REQUIRE(std::ranges::equal(evens, expected));
+}
+
+
+TEST_CASE("join strings")
+{
+    std::array<std::string, 3> arr = {"Hello", "World", "!"};
+    auto joined = arr | std::views::join;
+
+    // Convert initializer list to a range
+    std::string_view expected = "HelloWorld!";
+
+    auto result = std::accumulate(joined.begin(), joined.end(), std::string{}); // missing operator == for joined
+    REQUIRE(result == expected);
+}
+
+TEST_CASE("sum of squares")
+{
+   
+    const int N = 10;
+    const int EXPECTED = 385;
+    // iota(1) generates an infinite range starting from 1
+    // transform(square) squares each element
+    // take(N) takes the first N elements
+    // join flattens the nested ranges
+    auto sum_of_squares = std::views::iota(1) | std::views::transform(square) | std::views::take(N);
+
+    auto result = std::ranges::fold_left(sum_of_squares, 0, std::plus{});
+    REQUIRE(result == EXPECTED);
 }
