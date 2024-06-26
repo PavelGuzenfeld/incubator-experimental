234fabac1e15b95fe5161daf6ffe46fadb492de8
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2024-06-26 20:29:30 +0000

    Update CMakeLists.txt and add Timesync class for syncing UTC with PX4

--- sync-utc-px4/src/sync-utc-px4-timesync.cpp
+++ sync-utc-px4/src/sync-utc-px4-timesync.cpp
@@ -1,84 +1,15 @@
 #include <cstdint>
-#include "mavsdk_connector.hpp"
+#include <mavsdk/mavsdk.h>
 #include <mavsdk/plugins/mavlink_passthrough/mavlink_passthrough.h>
-
-class Timesync {
-public:
-    Timesync(mavsdk::MavlinkPassthrough& mavlink_passthrough);
-
-private:
-    void send_timesync_request();
-    int64_t calculate_offset() const;
-    int64_t local_time() const;
-    void on_timesync_message(const mavlink_message_t& message);
-
-    mavsdk::MavlinkPassthrough& _mavlink_passthrough;
-    int64_t _send_timestamp;
-    int64_t _receive_timestamp;
-    int64_t _offset;
-};
-
 #include <chrono>
 #include <iostream>
-
-Timesync::Timesync(mavsdk::MavlinkPassthrough& mavlink_passthrough)
-    : _mavlink_passthrough(mavlink_passthrough), _send_timestamp(0), _receive_timestamp(0), _offset(0) {
-    _mavlink_passthrough.subscribe_message(
-        MAVLINK_MSG_ID_TIMESYNC, [this](const mavlink_message_t& message) {
-            on_timesync_message(message);
-        });
-}
-
-void Timesync::send_timesync_request() {
-    _send_timestamp = local_time();
-    mavlink_timesync_t timesync ={0,0};    
-    mavlink_message_t message;
-    mavlink_msg_timesync_pack(
-        _mavlink_passthrough.get_our_sysid(),
-        _mavlink_passthrough.get_our_compid(),
-        &message,
-        timesync.tc1,
-        timesync.ts1);
-    auto result = _mavlink_passthrough.queue_message([&message](MavlinkAddress, uint8_t)
-                                                      { return message; });
-}
-
-void Timesync::on_timesync_message(const mavlink_message_t& message) {
-    mavlink_timesync_t timesync;
-    mavlink_msg_timesync_decode(&message, &timesync);
-
-    if (timesync.tc1 == 0) {
-        // this is a request, respond back
-        mavlink_message_t response;
-        mavlink_msg_timesync_pack(
-            _mavlink_passthrough.get_our_sysid(),
-            _mavlink_passthrough.get_our_compid(),
-            &response,
-            local_time(),
-            timesync.ts1);
-            auto result = _mavlink_passthrough.queue_message([&message](MavlinkAddress, uint8_t)
-                                                      { return message; });
-    } else {
-        // this is a response
-        _receive_timestamp = local_time();
-        _offset = ((timesync.tc1 - timesync.ts1) + (timesync.ts1 - _send_timestamp)) / 2;
-    }
-}
-
-int64_t Timesync::calculate_offset() const {
-    return _offset;
-}
-
-int64_t Timesync::local_time() const {
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(
-        std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-}
-
-#include <iostream>
+#include <memory>
 #include <thread>
+#include <functional>
+#include "mavsdk_connector.hpp"
 
 int main(int argc, char** argv) {
-     if (argc != 2) {
+    if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <connection_url>\n";
         return 1;
     }
@@ -90,18 +21,68 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+
+
     auto mavlink_passthrough = std::make_shared<mavsdk::MavlinkPassthrough>(system);
 
-    Timesync timesync{mavlink_passthrough};
+    //Test time is actually synchronized
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    //subscribe to system time
+    mavlink_passthrough->subscribe_message(
+        MAVLINK_MSG_ID_SYSTEM_TIME, [](const mavlink_message_t& message) {
+            mavlink_system_time_t controller_time;
+            mavlink_msg_system_time_decode(&message, &controller_time);
+            //Get current utc time in nanoseconds
+            auto current_time = std::chrono::high_resolution_clock::now();
+            auto offset = controller_time.time_unix_usec - std::chrono::duration_cast<std::chrono::microseconds>(current_time.time_since_epoch()).count();
+            std::cout << "Mavlink time utc: " << controller_time.time_unix_usec<< std::endl;
+            std::cout << "Mavlink time since boot: " << controller_time.time_boot_ms << std::endl;
+            std::cout << "Current time: " << std::chrono::duration_cast<std::chrono::microseconds>(current_time.time_since_epoch()).count() << std::endl;
+            std::cout << "Difference: " << offset << std::endl;
 
-    timesync.send_timesync_request();
+        });
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    mavlink_passthrough->subscribe_message(
+        MAVLINK_MSG_ID_TIMESYNC, [&](const mavlink_message_t& message) {
+            mavlink_timesync_t timesync;
+            mavlink_msg_timesync_decode(&message, &timesync);
 
-    int64_t offset = timesync.calculate_offset();
-    std::cout << "Estimated offset: " << offset << " nanoseconds\n";
+            if (timesync.tc1 == 0) {
+                // This is a request, respond back
+                mavlink_message_t response;
+                auto local_time_us = std::chrono::duration_cast<std::chrono::microseconds>(
+                        std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+                mavlink_msg_timesync_pack(
+                    mavlink_passthrough->get_our_sysid(),
+                    mavlink_passthrough->get_our_compid(),
+                    &response,
+                    //tc1: time sync counter
+                    local_time_us,
+                    //ts1: time sync timestamp
+                    timesync.ts1,
+                    message.sysid,
+                    message.compid);
+
+                auto result = mavlink_passthrough->queue_message([message](MavlinkAddress, uint8_t)
+                                                      { 
+                                                        return message; });
+                if (result != mavsdk::MavlinkPassthrough::Result::Success) {
+                    std::cerr << "Failed to send timesync response\n";
+                }
+            } else {
+                // This is a response
+                // receive_timestamp_ = local_time_ns();
+                // offset_ = ((timesync.tc1 - timesync.ts1) + (timesync.ts1 - send_timestamp_)) / 2;
+            }
+        });
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    std::cout<<"Enabling timesync"<<std::endl;
+        system->enable_timesync();
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+
+    std::cout << "Finished" << std::endl;
 
     return 0;
 }
