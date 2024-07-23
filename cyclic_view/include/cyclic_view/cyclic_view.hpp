e117827160311576b6861086f0e3d31324b1acf2
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2024-07-23 16:23:35 +0000

    Const iterator support added

--- cyclic_view/include/cyclic_view/cyclic_view.hpp
+++ cyclic_view/include/cyclic_view/cyclic_view.hpp
@@ -14,17 +14,17 @@ public:
     using iterator_category = std::forward_iterator_tag;
     using value_type = typename ContainerType::value_type;
     using difference_type = typename ContainerType::difference_type;
-    using pointer = typename ContainerType::pointer;
-    using reference = typename ContainerType::reference;
+    using pointer = typename std::conditional_t<std::is_const_v<ContainerType>, typename ContainerType::const_pointer, typename ContainerType::pointer>;
+    using reference = typename std::conditional_t<std::is_const_v<ContainerType>, typename ContainerType::const_reference, typename ContainerType::reference>;
 
     CyclicIterator(ContainerType& container, typename ContainerType::size_type start, typename ContainerType::size_type index)
         : container_(container), start_(start), index_(index) {}
 
-    reference operator*() {
+    reference operator*() const {
         return container_[(start_ + index_) % container_.size()];
     }
 
-    pointer operator->() {
+    pointer operator->() const {
         return &container_[(start_ + index_) % container_.size()];
     }
 
@@ -61,6 +61,7 @@ class CyclicView {
 public:
     using size_type = typename ContainerType::size_type;
     using iterator = CyclicIterator<ContainerType>;
+    using const_iterator = CyclicIterator<const ContainerType>;
 
     constexpr CyclicView(ContainerType& container, size_type size, size_type start = 0)
         : container_(container), start_(start), size_(size), push_index_(start) {
@@ -82,6 +83,14 @@ public:
 
     iterator end() {
         return iterator(container_, start_, size_);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(container_, start_, 0);
+    }
+
+    const_iterator end() const {
+        return const_iterator(container_, start_, size_);
     }
 
     void push(const typename ContainerType::value_type& value) {
@@ -108,6 +117,7 @@ public:
     using size_type = size_t;
     using value_type = T;
     using iterator = CyclicIterator<std::span<T>>;
+    using const_iterator = CyclicIterator<const std::span<T>>;
 
     constexpr CyclicView(T* array, size_type size)
         : container_(array, size), start_(0), size_(size), push_index_(0) {
@@ -128,6 +138,14 @@ public:
 
     iterator end() {
         return iterator(container_, start_, size_);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(container_, start_, 0);
+    }
+
+    const_iterator end() const {
+        return const_iterator(container_, start_, size_);
     }
 
     void push(const value_type& value) {
