af122d4fd5322779be79624ac93c917f62f9fbd0
Pavel Guzenfeld <me@pavelguzenfeld.com>
2024-07-23 23:16:59 +0000

    Add support for growing container in CyclicView

--- cyclic_view/include/cyclic_view/cyclic_view.hpp
+++ cyclic_view/include/cyclic_view/cyclic_view.hpp
@@ -1,175 +1,234 @@
 #pragma once
 
+#include <array>
 #include <cassert>
 #include <iterator>
 #include <span>
 #include <type_traits>
+#include <vector>
 
-namespace cyclic {
+namespace cyclic
+{
 
-// Iterator boilerplate using std::iterator
-template <typename ContainerType>
-class CyclicIterator {
-public:
-    using iterator_category = std::forward_iterator_tag;
-    using value_type = typename ContainerType::value_type;
-    using difference_type = typename ContainerType::difference_type;
-    using pointer = typename std::conditional_t<std::is_const_v<ContainerType>, typename ContainerType::const_pointer, typename ContainerType::pointer>;
-    using reference = typename std::conditional_t<std::is_const_v<ContainerType>, typename ContainerType::const_reference, typename ContainerType::reference>;
+    // Iterator boilerplate using std::iterator
+    template <typename ContainerType>
+    class CyclicIterator
+    {
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = typename ContainerType::value_type;
+        using difference_type = typename ContainerType::difference_type;
+        using pointer = typename std::conditional_t<std::is_const_v<ContainerType>, typename ContainerType::const_pointer, typename ContainerType::pointer>;
+        using reference = typename std::conditional_t<std::is_const_v<ContainerType>, typename ContainerType::const_reference, typename ContainerType::reference>;
 
-    CyclicIterator(ContainerType& container, typename ContainerType::size_type start, typename ContainerType::size_type index)
-        : container_(container), start_(start), index_(index) {}
+        CyclicIterator(ContainerType &container, typename ContainerType::size_type start, typename ContainerType::size_type index)
+            : container_(container), start_(start), index_(index) {}
 
-    reference operator*() const {
-        return container_[(start_ + index_) % container_.size()];
-    }
-
-    pointer operator->() const {
-        return &container_[(start_ + index_) % container_.size()];
-    }
-
-    CyclicIterator& operator++() {
-        ++index_;
-        return *this;
-    }
-
-    CyclicIterator operator++(int) {
-        CyclicIterator tmp = *this;
-        ++index_;
-        return tmp;
-    }
-
-    friend bool operator==(const CyclicIterator& a, const CyclicIterator& b) {
-        return a.index_ == b.index_ && &a.container_ == &b.container_;
-    }
-
-    friend bool operator!=(const CyclicIterator& a, const CyclicIterator& b) {
-        return !(a == b);
-    }
-
-    auto operator<=>(const CyclicIterator& other) const = default;
-
-private:
-    ContainerType& container_;
-    typename ContainerType::size_type start_;
-    typename ContainerType::size_type index_;
-};
-
-// Template class definition for standard containers
-template <typename ContainerType>
-class CyclicView {
-public:
-    using size_type = typename ContainerType::size_type;
-    using iterator = CyclicIterator<ContainerType>;
-    using const_iterator = CyclicIterator<const ContainerType>;
-
-    constexpr CyclicView(ContainerType& container, size_type size, size_type start = 0)
-        : container_(container), start_(start), size_(size), push_index_(start) {
-        assert(start < container.size() && "Invalid start index for CyclicView");
-        assert(size <= container.size() && "Invalid size for CyclicView");
-    }
-
-    [[nodiscard]] constexpr const auto& operator[](size_type index) const {
-        return container_[(start_ + index) % container_.size()];
-    }
-
-    [[nodiscard]] constexpr size_type size() const noexcept {
-        return size_;
-    }
-
-    iterator begin() {
-        return iterator(container_, start_, 0);
-    }
-
-    iterator end() {
-        return iterator(container_, start_, size_);
-    }
-
-    const_iterator begin() const {
-        return const_iterator(container_, start_, 0);
-    }
-
-    const_iterator end() const {
-        return const_iterator(container_, start_, size_);
-    }
-
-    void push(const typename ContainerType::value_type& value) {
-        container_[push_index_] = value;
-        push_index_ = (push_index_ + 1) % container_.size();
-        if (size_ < container_.size()) {
-            ++size_;
-        } else {
-            start_ = (start_ + 1) % container_.size();
+        reference operator*() const
+        {
+            return container_[(start_ + index_) % container_.size()];
         }
-    }
 
-private:
-    ContainerType& container_;
-    size_type start_;
-    size_type size_;
-    size_type push_index_;
-};
-
-// Specialization for C-style pointers
-template <typename T>
-class CyclicView<T*> {
-public:
-    using size_type = size_t;
-    using value_type = T;
-    using iterator = CyclicIterator<std::span<T>>;
-    using const_iterator = CyclicIterator<const std::span<T>>;
-
-    constexpr CyclicView(T* array, size_type size)
-        : container_(array, size), start_(0), size_(size), push_index_(0) {
-        assert(array != nullptr && "Invalid array pointer for CyclicView");
-    }
-
-    [[nodiscard]] constexpr const auto& operator[](size_type index) const {
-        return container_[(start_ + index) % size_];
-    }
-
-    [[nodiscard]] constexpr size_type size() const noexcept {
-        return size_;
-    }
-
-    iterator begin() {
-        return iterator(container_, start_, 0);
-    }
-
-    iterator end() {
-        return iterator(container_, start_, size_);
-    }
-
-    const_iterator begin() const {
-        return const_iterator(container_, start_, 0);
-    }
-
-    const_iterator end() const {
-        return const_iterator(container_, start_, size_);
-    }
-
-    void push(const value_type& value) {
-        container_[push_index_] = value;
-        push_index_ = (push_index_ + 1) % size_;
-        if (size_ < size_) {
-            ++size_;
-        } else {
-            start_ = (start_ + 1) % size_;
+        pointer operator->() const
+        {
+            return &container_[(start_ + index_) % container_.size()];
         }
-    }
 
-private:
-    std::span<T> container_;
-    size_type start_;
-    size_type size_;
-    size_type push_index_;
-};
+        CyclicIterator &operator++()
+        {
+            ++index_;
+            return *this;
+        }
 
-// Deduction guide
-template <typename ContainerType>
-CyclicView(ContainerType&, typename ContainerType::size_type, typename ContainerType::size_type) -> CyclicView<ContainerType>;
+        CyclicIterator operator++(int)
+        {
+            CyclicIterator tmp = *this;
+            ++index_;
+            return tmp;
+        }
 
-template <typename T>
-CyclicView(T*, size_t) -> CyclicView<T*>;
+        friend bool operator==(const CyclicIterator &a, const CyclicIterator &b)
+        {
+            return a.index_ == b.index_ && &a.container_ == &b.container_;
+        }
+
+        friend bool operator!=(const CyclicIterator &a, const CyclicIterator &b)
+        {
+            return !(a == b);
+        }
+
+        auto operator<=>(const CyclicIterator &other) const = default;
+
+    private:
+        ContainerType &container_;
+        typename ContainerType::size_type start_;
+        typename ContainerType::size_type index_;
+    };
+
+    // Template class definition for standard containers
+    template <typename ContainerType>
+    class CyclicView
+    {
+    public:
+        using size_type = typename ContainerType::size_type;
+        using iterator = CyclicIterator<ContainerType>;
+        using const_iterator = CyclicIterator<const ContainerType>;
+
+        constexpr CyclicView(ContainerType &container, size_type capacity, size_type start = 0)
+            : container_(container), capacity_(capacity), start_(start), size_(container.size()), push_index_(start)
+        {
+            assert(start < size_ && "Invalid start index for CyclicView");
+        }
+
+        [[nodiscard]] constexpr const auto &operator[](size_type index) const
+        {
+            return container_[(start_ + index) % capacity_];
+        }
+
+        [[nodiscard]] constexpr size_type size() const noexcept
+        {
+            return container_.size();
+        }
+
+        iterator begin()
+        {
+            return iterator(container_, start_, 0);
+        }
+
+        iterator end()
+        {
+            return iterator(container_, start_, size_);
+        }
+
+        const_iterator begin() const
+        {
+            return const_iterator(container_, start_, 0);
+        }
+
+        const_iterator end() const
+        {
+            return const_iterator(container_, start_, size_);
+        }
+
+        void push(const typename ContainerType::value_type &value)
+        {
+            if (container_.size() < capacity_)
+            {
+                insert_element(container_, container_.size() + push_index_, value);
+            }
+            else
+            {
+                container_[push_index_] = value;
+                start_ = (start_ + 1) % container_.size();
+            }
+            push_index_ = (push_index_ + 1) % container_.size();
+        }
+
+    private:
+        template <typename C>
+        void insert_element(C &container, size_type index, const typename C::value_type &value)
+        {
+           auto result = container.insert(container.begin() + index, value);
+           if (result == container.end())
+           {
+               container.insert(container.end(), value);
+           }
+        }
+
+        template <typename T, size_t N>
+        void insert_element(std::array<T, N> &container, size_type index, const T &value)
+        {
+            container[index] = value;
+        }
+
+        // bool invariant() const
+        // {
+        //     return start_ < capacity_ && size_ <= container_ && push_index_ < container_.size() && container_.size() <= capacity_;
+        // }
+
+        ContainerType &container_;
+        size_type capacity_;
+        size_type start_;
+        size_type size_;
+        size_type push_index_;
+    };
+
+    // Specialization for C-style pointers
+    template <typename T>
+    class CyclicView<T *>
+    {
+    public:
+        using size_type = size_t;
+        using value_type = T;
+        using iterator = CyclicIterator<std::span<T>>;
+        using const_iterator = CyclicIterator<const std::span<T>>;
+
+        constexpr CyclicView(T *array, size_type capacity, size_type size = 0, size_type start = 0)
+            : container_(array, capacity), capacity_(capacity), start_(start), size_(size), push_index_(size % capacity)
+        {
+            assert(array != nullptr && "Invalid array pointer for CyclicView");
+            assert(start < capacity && "Invalid start index for CyclicView");
+            assert(size <= capacity && "Size exceeds capacity");
+        }
+
+        [[nodiscard]] constexpr const auto &operator[](size_type index) const
+        {
+            return container_[(start_ + index) % capacity_];
+        }
+
+        [[nodiscard]] constexpr size_type size() const noexcept
+        {
+            return size_;
+        }
+
+        iterator begin()
+        {
+            return iterator(container_, start_, 0);
+        }
+
+        iterator end()
+        {
+            return iterator(container_, start_, size_);
+        }
+
+        const_iterator begin() const
+        {
+            return const_iterator(container_, start_, 0);
+        }
+
+        const_iterator end() const
+        {
+            return const_iterator(container_, start_, size_);
+        }
+
+        void push(const value_type &value)
+        {
+            assert(size_ <= capacity_ && "Cannot push beyond capacity for C-style array");
+            container_[push_index_] = value;
+            push_index_ = (push_index_ + 1) % size_;
+            if (size_ < capacity_)
+            {
+                ++size_;
+            }
+            else
+            {
+                start_ = (start_ + 1) % size_;
+            }
+        }
+
+    private:
+        std::span<T> container_;
+        size_type capacity_;
+        size_type start_;
+        size_type size_;
+        size_type push_index_;
+    };
+
+    // Deduction guide
+    template <typename ContainerType>
+    CyclicView(ContainerType &, typename ContainerType::size_type, typename ContainerType::size_type) -> CyclicView<ContainerType>;
+
+    template <typename T>
+    CyclicView(T *, size_t, size_t) -> CyclicView<T *>;
 
 } // namespace cyclic
--- cyclic_view/test/test_cyclic_view.cpp
+++ cyclic_view/test/test_cyclic_view.cpp
@@ -4,7 +4,8 @@
 
 using namespace cyclic;
 
-TEST_CASE("CyclicView with std::array") {
+TEST_CASE("CyclicView with std::array")
+{
     std::array<int, 5> arr = {1, 2, 3, 4, 5};
     CyclicView view(arr, arr.size(), 0);
 
@@ -45,23 +46,25 @@ TEST_CASE("CyclicView with std::array") {
 TEST_CASE("CyclicView with C-style array") {
     int c_arr[] = {6, 7, 8, 9, 10};
     std::size_t size = sizeof(c_arr) / sizeof(c_arr[0]);
-    CyclicView view(c_arr, size);
+    CyclicView view(c_arr, size, size);
 
     std::vector<int> result(view.begin(), view.end());
     REQUIRE(result == std::vector<int>{6, 7, 8, 9, 10});
 }
 
-TEST_CASE("CyclicView with std::vector") {
+TEST_CASE("CyclicView with std::vector")
+{
     std::vector<int> vec = {11, 12, 13, 14, 15};
-    CyclicView view_vec(vec, vec.size(), 0);
+    CyclicView view_vec(vec, vec.size());
 
     std::vector<int> result(view_vec.begin(), view_vec.end());
     REQUIRE(result == std::vector<int>{11, 12, 13, 14, 15});
 }
 
-TEST_CASE("Pushing data into CyclicView") {
+TEST_CASE("Pushing data into CyclicView")
+{
     std::array<int, 3> push_arr = {0, 0, 0};
-    CyclicView push_view(push_arr, 0, 0);
+    CyclicView push_view(push_arr, push_arr.size());
 
     // Pushing elements
     push_view.push(1);
@@ -70,5 +73,32 @@ TEST_CASE("Pushing data into CyclicView") {
     push_view.push(4); // This should overwrite the first element
 
     std::vector<int> result(push_view.begin(), push_view.end());
-    REQUIRE(result == std::vector<int>{ 2, 3, 4});
+    REQUIRE(result == std::vector<int>{2, 3, 4});
+}
+
+TEST_CASE("CyclicView with growing container")
+{
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    CyclicView view(vec, 6);
+
+    // Initial view check
+    std::vector<int> result(view.begin(), view.end());
+    REQUIRE(result == std::vector<int>{1, 2, 3, 4, 5});
+
+    // Grow the underlying container
+    vec.push_back(6);
+    REQUIRE(vec.size() == 6);
+    REQUIRE(vec == std::vector<int>{1, 2, 3, 4, 5, 6});
+
+    // View should now handle the larger container correctly
+    view.push(7);
+    auto result_push_7 = {2, 3, 4, 5, 6, 7};
+    REQUIRE(view.size() == 6);
+    REQUIRE(std::equal(result_push_7.begin(), result_push_7.end(), view.begin()));
+
+    // Push more elements
+    view.push(8);
+    auto result_push_8 = {3, 4, 5, 6, 7, 8};
+    REQUIRE(view.size() == 6);
+    REQUIRE(std::equal(result_push_8.begin(), result_push_8.end(), view.begin()));
 }
